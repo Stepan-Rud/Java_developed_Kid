4ea8bf66e0e2c0620e0e106b9a5a5bc1f8b68a4b
RudStepan <1gooder2@gmail.com>
2024-05-29 09:05:04 +0700

    save2 dz2

---
+++ src/Sem2Dz/Main.java
@@ -0,0 +1,13 @@
+package Sem2Dz;
+
+import Dz.ClientGUI;
+import Sem2Dz.client.ClientController;
+import Sem2Dz.client.ClientGUII;
+import Sem2Dz.server.ServerWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        ServerWindow serverWindow = new ServerWindow();
+        ClientGUII clientGUII = new ClientGUII();
+    }
+}
